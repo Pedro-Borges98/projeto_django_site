942aaa726146c30a3ecb156aed2b7f63851b34c2
Pedro Borges <pedroborges@usp.br>
2024-02-16 17:06:15 -0300

    Changes to be committed: 	modified:   db.sqlite3 	new file:   galeria/migrations/0001_initial.py 	modified:   galeria/models.py 	modified:   galeria/views.py 	modified:   templates/galeria/index.html

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# essa classe se torna um model pq representa uma tabela no banco de dados
+class Fotografia(models.Model):
+    nome      = models.CharField(max_length = 100, null = False, blank = False)
+    legenda   = models.CharField(max_length = 150, null = False, blank = False)
+    descricao = models.TextField(null = False, blank = False)
+    foto      = models.CharField(max_length = 100,null = False, blank = False)
+
+    def __str__(self):
+        return f"Fotografia [nome={self.nome}]"
